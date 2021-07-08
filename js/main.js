618327db3d7610fab5f4c8441ecf7fdc22e440f1
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2021-07-08 12:28:35 +0600

    not complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+// Magnific popup for image
+
 $(document).ready(function () {
-  $(".image-link").magnificPopup({
+  $(".project-popup").magnificPopup({
     type: "image",
     gallery: {
       enabled: true,
@@ -7,8 +9,29 @@ $(document).ready(function () {
   });
 });
 
-// $(".parent-container").magnificPopup({
-//   delegate: "a", // child items selector, by clicking on it popup will open
-//   type: "image",
-//   // other options
-// });
+// Magnific popup for video
+
+$(document).ready(function () {
+  $(".video").magnificPopup({
+    type: "iframe",
+  });
+});
+
+// Counter js
+jQuery(document).ready(function ($) {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+});
+
+// Slick Slider js
+
+$(document).ready(function () {
+  $(".slick_slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><i class="fas fa-arrow-right"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><i class="fas fa-arrow-left"></i></button>',
+  });
+});
